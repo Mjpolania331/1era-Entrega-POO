a2beac07ce56f54ef1af1ded1d3e9e6d109d1c17
Mjpolania331 <mpolania331@soyudemedellin.edu.co>
2024-11-02 03:38:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class JuegoGUI:
         self.master.title("Juego de Memoria")
         self.master.geometry("500x300")  
 
-        self.label = tk.Label(self.master, text="¡Bienvenido al Juego de Memoria!", font=("Arial", 14))
+        self.label = tk.Label(self.master, text="Bienvenido al Juego de Memoria", font=("Arial", 15))
         self.label.pack(pady=20)
 
         self.boton = tk.Button(self.master, text="Iniciar Juego", command=self.iniciar_juego)
